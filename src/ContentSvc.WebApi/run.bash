ce2d13d11bda015848e9978645f4b2ea9f4162e8
Zhen Chen <redo.chenzhen@hotmail.com>
2021-07-29 15:59:42 +0800

    bugfix: run.bash

--- src/ContentSvc.WebApi/run.bash
+++ src/ContentSvc.WebApi/run.bash
@@ -6,5 +6,5 @@ chmod a+x "${SCRIPT_DIR}/mc"
 
 #####################
 #TODO: necessary?
-#yum install -y cabextract fontconfig xorg-x11-font-utils
-yum install -y  https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+#yum install -y cabextract fontconfig xorg-x11-font-utils || true
+yum install -y  https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm || true
